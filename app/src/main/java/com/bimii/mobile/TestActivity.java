9bcd46913d1ccf28c9bcb68974fb4ba3bc2b0fa2
Michael Soyma <matanaliz93@mail.ru>
2015-09-01 10:25:33 +0300

    Initial commit

---
+++ app/src/main/java/com/bimii/mobile/TestActivity.java
@@ -0,0 +1,18 @@
+package com.bimii.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Asus_Dev on 8/31/2015.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+}
