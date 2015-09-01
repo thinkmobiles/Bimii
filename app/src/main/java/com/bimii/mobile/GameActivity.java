25e54244e2a4f1e39be1a8c5123742614262b57a
Michael Soyma <matanaliz93@mail.ru>
2015-09-01 10:42:44 +0300

    Config Readme and add retrofit gradle dependencies

--- app/src/main/java/com/bimii/mobile/GameActivity.java
+++ app/src/main/java/com/bimii/mobile/GameActivity.java
@@ -2,17 +2,16 @@ package com.bimii.mobile;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
 /**
  * Created by Asus_Dev on 8/31/2015.
  */
-public class TestActivity extends Activity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_games);
     }
 
 }
